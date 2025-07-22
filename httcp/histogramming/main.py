aef320d027c553e8bedc0f76368d22acbf4496df
Aliya Nigamova <aliya.nigamova@cern.ch>
2025-07-22 17:58:25 +0200

    adding the first version of hist producer

---
+++ httcp/histogramming/main.py
@@ -0,0 +1,101 @@
+# Decompiled with PyLingual (https://pylingual.io)
+# Internal filename: /afs/cern.ch/work/a/anigamov/DesyTau/CPinHToTauTau/httcp/histogramming/main.py
+# Bytecode version: 3.9.0beta5 (3425)
+# Source timestamp: 2025-07-22 15:21:18 UTC (1753197678)
+
+"""
+Example event weight producer.
+"""
+import law
+import order as od
+from columnflow.histogramming import HistProducer, hist_producer
+from columnflow.histogramming.default import cf_default, create_hist_from_variables, fill_hist, translate_hist_intcat_to_strcat
+from columnflow.columnar_util import Route
+from columnflow.util import maybe_import, pattern_matcher
+from columnflow.types import Any
+np = maybe_import('numpy')
+ak = maybe_import('awkward')
+hist = maybe_import('hist')
+
+@cf_default.hist_producer(keep_weights=None, skip_compatibility_check=True, drop_weights={'normalization_weight_inclusive'})
+def default(self: HistProducer, events: ak.Array, **kwargs) -> ak.Array:
+    processes = self.dataset_inst.processes.names()
+    weight = ak.Array(np.ones(len(events), dtype=np.float32))
+    for column in self.weight_columns:
+        weight = weight * Route(column).apply(events)
+    weight_dict = {}
+    for cat in self.config_inst.categories.names():
+        category = self.config_inst.get_category(cat)
+        if 'apply_ff' not in category.aux.keys():
+            weight_dict[category] = weight
+        elif category.aux['apply_ff'] == 'wj':
+            self.publish_message(f'applying FF weights: ff_weight_wj_nominal, category: {category.name}')
+            weight_dict[category] = weight * events.ff_weight_wj_nominal
+        elif category.aux['apply_ff'] == 'qcd':
+            self.publish_message(f'applying FF weights: ff_weight_qcd_nominal, category: {category.name}')
+            weight_dict[category] = weight * events.ff_weight_qcd_nominal
+    return (events, weight_dict)
+
+@default.init
+def default_init(self: HistProducer) -> None:
+    self.weight_columns = set()
+    do_keep = pattern_matcher(self.keep_weights) if self.keep_weights else lambda _, /: True
+    do_drop = pattern_matcher(self.drop_weights) if self.drop_weights else lambda _, /: False
+    all_weights = self.config_inst.x.event_weights.copy()
+    all_weights.update(self.dataset_inst.x('event_weights', {}))
+    for weight_name, shift_insts in all_weights.items():
+        if not do_keep(weight_name) or do_drop(weight_name):
+            continue
+        self.weight_columns.add(weight_name)
+        self.uses.add(weight_name)
+        self.shifts |= {shift_inst.name for shift_inst in shift_insts}
+
+@default.create_hist
+def default_create_hist(self: HistProducer, variables: list[od.Variable], task: law.Task, **kwargs) -> dict:
+    """
+    Define the histogram structure for the default histogram producer.
+    Returns a dictionary of hist.Histogram objects keyed by category.
+    """
+    histograms = {}
+    for category in self.config_inst.get_leaf_categories():
+        histograms[category] = create_hist_from_variables(*variables, categorical_axes=(('process', 'intcat'), ('shift', 'intcat')), weight=True)
+    return histograms
+
+@default.fill_hist
+def default_fill_hist(self: HistProducer, h: dict, data: dict[str, Any], task: law.Task) -> None:
+    """
+    Fill the histogram with the data.
+    """
+    data = {k: v for k, v in data.items() if k != 'category'}
+    weight_dict = data['weight']
+    for category in self.config_inst.get_leaf_categories():
+        data['weight'] = weight_dict[category]
+        fill_hist(h[category], data, last_edge_inclusive=task.last_edge_inclusive)
+
+@default.post_process_hist
+def default_post_process_hist(self: HistProducer, h_dict: dict, task: law.Task) -> dict:
+    """
+    Post-process the his togram, converting integer to string axis for consistent lookup across configs where ids might
+    be different.
+    """
+    hist = {}
+    for category in self.config_inst.get_leaf_categories():
+        h = h_dict[category]
+        axis_names = {ax.name for ax in h.axes}
+        if 'process' in axis_names:
+            process_map = {proc_id: self.config_inst.get_process(proc_id).name for proc_id in h.axes['process']}
+            h = translate_hist_intcat_to_strcat(h, 'process', process_map)
+        if 'shift' in axis_names:
+            shift_map = {task.global_shift_inst.id: task.global_shift_inst.name}
+            h = translate_hist_intcat_to_strcat(h, 'shift', shift_map)
+        hist[category] = h
+    return hist
+
+@default.post_process_merged_hist
+def post_process_merged_hist_func(self: HistProducer, h_dict: dict, task: law.Task) -> None:
+    """
+    Post-process the his togram, converting integer to string axis for consistent lookup across configs where ids might
+    be different.
+    """
+    hist = {}
+    print(h_dict)
